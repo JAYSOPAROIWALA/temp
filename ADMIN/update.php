6ae79bdf8b2ba13a447529bfa4c31d53240cf9d9
JAYSOPAROIWALA <111558318+JAYSOPAROIWALA@users.noreply.github.com>
2024-01-17 11:57:12 +0530

    second time

--- ADMIN/update.php
+++ ADMIN/update.php
@@ -100,7 +100,7 @@
               <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
 
                 <img src="../img/draw1.webp"
-                  class="img-fluid" alt="Sample image">
+                  class="img-fluid" alt="Sample images">
 
               </div>
             </div>
